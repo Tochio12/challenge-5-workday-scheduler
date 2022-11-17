65427c6f3e081ae10fb45f2fe3e685dfd3836ad2
Melany Baldeon <tochio12910@gmail.com>
2022-11-17 18:01:47 -0500

    fixed some minor bugs

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
 
 $(document).ready(function () {
     // saveBtn click listener
-    $(".saveButton").on("click", function () {
+    $(".saveBtn").on("click", function () {
      // get nearby values of the description in JQuery
         var value = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
@@ -41,5 +41,16 @@ $(document).ready(function () {
     }
 
     timeUpdater();
+
+    // get items from local storage if any
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
 });
